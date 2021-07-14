68a42093a64e83842b0ad45a5956caac6d8957a0
Halogem <aspenp02@outlook.com>
2021-07-14 17:51:58 -0400

    Optimized
    More optimizations~

--- root/main.cpp
+++ root/main.cpp
@@ -8,12 +8,24 @@
 #include <string>
 #include <fstream>
 #include "includes/calc/calc_alg.h"
-#include "includes/txt/txt.h"
+// #include "includes/txt/txt.h"
 using namespace std;
 #define NewL '\n' // To simplify making new lines between multiple 'cout' phrases rather than using '\n' so many times.
 
 int main() {
 
+    cout << NewL;
+    string doggo;
+        ifstream dogascii ("txt/doggo.txt");
+        if(dogascii.is_open () ) {
+            while (! dogascii.eof() ) {
+                getline (dogascii,doggo);
+                cout << doggo << endl;
+            }
+        dogascii.close();
+    }
+
+    cout << NewL;
     cout << "┌────────────────────────┐" << endl;
     cout << "│                        │" << endl;
     cout << "│   ⚝ The Ultimate ⚝     │" << endl;
@@ -23,62 +35,34 @@ int main() {
     cout << "│                        │" << endl;
     cout << "┕━━━━━━━━━━━━━━━━━━━━━━━━┙" << endl;
 
-/*
-    string doggo;
-            ifstream dogascii ("doggo.txt");
-
-            if(dogascii.is_open () ) {
-
-                while (! dogascii.eof() ) {
-
-                    getline (dogascii,doggo);
-                    cout << doggo << endl;
-
-                }
-
-                dogascii.close();
-
-            }
-*/
-
-    cout << dogart() << endl;
-
     cout << "Open-Source Developed by Aspen, aka. Halogem!" << endl;
     cout << "For command usage, please type 'help' and press Enter! " << endl;
     cout << "NOTE: If you enter improper values in application calc, you may break this application. Will be fixed down the line." << endl;
     cout << NewL;
 
-    /*
-	User Interaction
-	Begins Below!
-    */
-
+    // USER INTERACTION BEGINS BELOW
     string temp;
     while(1 == 1) {
 
-interaction_beginning:
+// interaction_beginning:
         cout << ">> ";
         cin >> temp;
 
         if(temp == "help") {
-
             string line;
-            ifstream title ("help.txt");
+            ifstream title ("txt/help.txt");
 
             if(title.is_open () ) {
-
                 while (! title.eof() ) {
 
                     getline (title,line);
                     cout << line << endl;
 
                 }
-
                 title.close();
-                goto interaction_beginning;
+                // goto interaction_beginning;
 
             }
-        
         }
 
         if (temp == "secrets_uwu") {
@@ -87,59 +71,45 @@ interaction_beginning:
             
         }
 
-        while(temp == "calc") {
-
-            // CALCULATOR EXTENSION
-       
-            cout << NewL << "         [ Basic Calculator ]" << endl;
+        if(temp == "calc") {
+            // CALCULATOR EXTENSION       
+            cout << "┌────────────────────────┐" << endl;
+            cout << "│    Basic Calculator    │" << endl;
+            cout << "┕━━━━━━━━━━━━━━━━━━━━━━━━┙" << endl;
 
             char calc_o; // operator
             int calc_a;
             int calc_b;
             int calc_c; // result
-        
+            
             cout << "Number" << NewL << ">> ";
             cin >> calc_a;
 
             while(cin.good()) {
-
                 cout << "Operator (+, -, *, /)" << NewL << ">> ";
                 cin >> calc_o;
 
-                if(calc_o == '+') {
-
+                while(calc_o == '+') {
                     cout << "Number" << NewL << ">> ";
                     cin >> calc_b;
 
                     if(cin.fail()) {
-
                         std::cout << NewL << "Please try again! " << endl;
                     
                     } else {
-
                         //  calc_c = (calc_a, calc_b);
                         calc_c = add(calc_a, calc_b);
                         cout << "Final Added Result" << NewL << "== " << calc_c << endl;
                         cout << "[!] Returning to The Ultimate Furry Terminal..." << NewL << endl; 
-                        goto interaction_beginning;
+                        // goto interaction_beginning;
                     
                     }
-
                 }
-
                 if(calc_o == '-') {
-
                     cout << "Number" << NewL << ">> ";
                     cin >> calc_b;
 
-                    /*
-
-                        Need to add error checking for improper data types.
-                
-                    */
-
                     if(cin.fail()) {
-
                         std::cout << NewL << "Please try again! " << endl;
                     
                     } else {
@@ -147,65 +117,50 @@ interaction_beginning:
                         calc_c = sub(calc_a, calc_b);
                         cout << "Final Subtracted Result" << NewL << "== " << calc_c << endl;
                         cout << "[!] Returning to The Ultimate Furry Terminal..." << NewL << endl; 
-                        goto interaction_beginning;
+                        // goto interaction_beginning;
                     
                     }
-
                 }
 
                 if(calc_o == '*') {
-
                     cout << "Number" << NewL << ">> ";
                     cin >> calc_b;
 
                     if(cin.fail()) {
-
                         std::cout << NewL << "Please try again! " << endl;
                     
                     } else {
-
                         // calc_c = (calc_a, calc_b);
                         calc_c = add(calc_a, calc_b);
                         cout << "Final Multiplied Result" << NewL << "== " << calc_c << endl;
                         cout << "[!] Returning to The Ultimate Furry Terminal..." << NewL << endl; 
-                        goto interaction_beginning;
+                        // goto interaction_beginning;
                     
                     }
-
                 }
 
                 if(calc_o == '/') {
-
                     cout << "Number" << NewL << ">> ";
                     cin >> calc_b;
 
                     if(cin.fail()) {
-
                         std::cout << NewL << "Please try again! " << endl;
                     
                     } else {
-
                         // calc_c = (calc_a, calc_b);
                         calc_c = add(calc_a, calc_b);
                         cout << "Final Divided Result" << NewL << "== " << calc_c << endl;
                         cout << "[!] Returning to The Ultimate Furry Terminal..." << NewL << endl; 
-                        goto interaction_beginning;
+                        // goto interaction_beginning;
                     
                     }
-
                 }
-
-            
             }
-
-        
         }
-	
     }
 
 
     // other interactions to come soon.
     cin.get(); 
-
 
 }
